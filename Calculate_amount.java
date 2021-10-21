0680541dea911b795d66bb5fdd6b78633cd8b42b
AvishekChatterj <92716035+AvishekChatterj@users.noreply.github.com>
2021-10-21 13:17:52 +0530

    Update Calculate_amount.java

--- Calculate_amount.java
+++ Calculate_amount.java
@@ -7,21 +7,27 @@ public class Calculate_amount
 	public static void main(String[] args) 
 	{
 		Scanner sc=new Scanner(System.in);
-		System.out.println("Enter your budget: ");
-		int budget=sc.nextInt();
-		System.out.println("Enter the amount spent on: ");
-		System.out.println("Food: ");
-		int food=sc.nextInt();
-		System.out.println("Toiletries: ");
-		int toiletries=sc.nextInt();
-		System.out.println("Luxury Items: ");
-		int luxury_items=sc.nextInt();
-		System.out.println("OTT platforms: ");
-		int ott_bill=sc.nextInt();
-		int sum=food+luxury_items+toiletries+ott_bill;
+		System.out.print("Enter your budget: ");
+			int budget=sc.nextInt();
+		System.out.println("Enter the amout spent on: ");
+		System.out.print("Food: ");
+			int food=sc.nextInt();
+		System.out.print("Toiletries: ");
+			int toiletries=sc.nextInt();
+		System.out.print("Luxury Items: ");
+			int luxury_items=sc.nextInt();
+		System.out.print("OTT platforms: ");
+			int ott_bill=sc.nextInt();
+			
+		int total_expense=food+luxury_items+toiletries+ott_bill;
 		
-		System.out.println("You have spent "+sum+" out of "+budget);
-		System.out.println("Percentage: "+ (sum/(float)budget)*100);
+		System.out.println("You have spent "+total_expense+" out of "+budget);
+		if(total_expense==budget)
+			System.out.println("You have reached 100% your budget ");
+		else if(total_expense>budget)
+			System.out.println("You have over-expended your budget");
+		else
+		System.out.println("You have reached"+ (total_expense/(float)budget)*100+" of your budget");
 		sc.close();
 	}
 }
